01a320f80372f4e8935731ff6de5c4c6595eea6c
lisaazg <62955237+lisaazg@users.noreply.github.com>
2022-04-12 18:41:46 +0200

    Update exo_03.rb

--- exo_03.rb
+++ exo_03.rb
@@ -1,3 +1,5 @@
-puts "Hello monde!"
-## message = "Hello monde!" 
-puts "Et avec une voix sexy, ça donne :" + message + ""
+puts "Quel est votre année de naissance ?"
+year_of_birth = gets.chomp.to_i
+
+age = 2022 - year_of_birth
+puts "En 2017 tu avais #{age - 5} ans"
