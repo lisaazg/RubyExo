d5402d39c4d1f8f7908d6ff6a6dd6c96b90299ca
lisaazg <62955237+lisaazg@users.noreply.github.com>
2022-04-12 18:49:45 +0200

    Update exo_06.rb

--- exo_06.rb
+++ exo_06.rb
@@ -1,7 +1,5 @@
-number_of_hours_worked_per_day = 8
-number_of_days_worked_per_week = 5
-number_of_weeks_in_THP = 2
-number_of_minutes_in_an_hour = 50
-
-puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
-puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+puts "Ecris un chiffre ?"
+nombre = gets.chomp.to_i
+nombre.times do 
+    puts "Salut, ca farte ?"
+end    
