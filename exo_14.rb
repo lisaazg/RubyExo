0d3735a7adf6352df6b24a9c1b2936a685b6cedd
lisaazg <62955237+lisaazg@users.noreply.github.com>
2022-04-13 08:38:24 +0200

    Create exo_14.rb

---
+++ exo_14.rb
@@ -0,0 +1,13 @@
+number = 0
+email = []
+
+while number <= 50
+    email1 = "jeandupont#{number += 1}@email.fr"
+    email << "#{email1}"
+    
+     if number % 2 == 0
+       puts email [number-1] #pour commencer à 1
+    end
+    
+
+end
