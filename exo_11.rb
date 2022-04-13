dc3086515f052776e6dabd573e4035bd77142f6c
lisaazg <62955237+lisaazg@users.noreply.github.com>
2022-04-13 08:34:58 +0200

    Update exo_11.rb

--- exo_11.rb
+++ exo_11.rb
@@ -1,1 +1,12 @@
-## FONCTIONNE PAS
+puts "Quelle est votre année de naissance ?"
+year_of_birth = gets.chomp.to_i
+
+age = 2022 - year_of_birth.to_i
+agereverse = 0
+puts year_of_birth 
+puts 0
+age.times do 
+    puts year_of_birth += 1  
+    puts agereverse += 1
+    
+end  
