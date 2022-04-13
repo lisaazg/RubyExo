0b766f564176551c0cc30314302d200905458961
lisaazg <62955237+lisaazg@users.noreply.github.com>
2022-04-13 08:15:07 +0200

    Update exo_07.rb

--- exo_07.rb
+++ exo_07.rb
@@ -1,4 +1,9 @@
-puts "Bonjour, c'est quoi ton blase ?"
-user_name = gets.chomp
-puts user_name
-## CA NE FONCTIONNE PAS
+puts "Ecris un nombre ?"
+nombre = gets.chomp.to_i
+
+total = 
+puts total
+nombre.times do 
+    total += 1
+    puts total.to_s    
+end    
